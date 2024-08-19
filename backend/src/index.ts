7e32eb6200328a26564fce7aeea8e42ffce925b3
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-08-20 01:12:10 +0530

    Migration and Client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
 app.get('/', (c) => {
+  
+
   return c.text('Motherfu*ker!')
 })
 
