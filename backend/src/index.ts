87641671fe1ef83e54be1d3119fbad7f10ab8d32
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-08-19 23:27:21 +0530

    Accelerate Connection Pool

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,28 @@ app.get('/', (c) => {
   return c.text('Motherfu*ker!')
 })
 
-app.get('/api/v1/user/signup', (c) => {
-  return c.json({
-    msg: "BRO ?" 
-  })
+app.post('/api/v1/signup', (c) => {
+  return c.text('signup')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('post - blog')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('put - blog')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('get - blog')
+})
+
+app.get('/api/v1/blogs', (c) => {
+  return c.text('all the blogs!')
 })
 
 export default app
