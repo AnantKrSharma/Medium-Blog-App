24fb378922c128ee10088a2d6cfde06009f4f7b9
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-08-21 01:16:11 +0530

    added JWT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,61 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { env } from 'hono/adapter'
+import { sign, decode, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>();
 
-app.get('/', (c) => {
-  
 
-  return c.text('Motherfu*ker!')
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  const response = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  const token = sign({ id: response.id }, c.env.JWT_SECRET)
+
+  return c.json({
+    jwt: token
+  })
 })
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('signup')
-})
 
 app.post('/api/v1/signin', (c) => {
   return c.text('signin')
 })
 
+
 app.post('/api/v1/blog', (c) => {
   return c.text('post - blog')
 })
+
 
 app.put('/api/v1/blog', (c) => {
   return c.text('put - blog')
 })
 
+
 app.get('/api/v1/blog/:id', (c) => {
   return c.text('get - blog')
 })
 
+
 app.get('/api/v1/blogs', (c) => {
   return c.text('all the blogs!')
 })
+
 
 export default app
